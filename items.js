2e506d35d930f105c327dc9cb1bdf77f4a9da62f
Khairi Ross <rosskhairi2@gmail.com>
2025-04-14 10:26:42 -0400

    Added items.js and marketplace.js

---
+++ items.js
@@ -0,0 +1,11 @@
+// Written by Jordan A. McCloud
+// Item JSON objects.
+let itemJSON = [
+    {itemID: "001", price: "5", name: "Test Item #1", description: "An item for testing."},
+    {itemID: "002", price: "10", name: "Test Item #2", description: "An item for testing."}
+];
+// Captian JSON objects.
+let captainJSON = [
+    {captainID: "001", price: "25", name: "Test Captain #1", description: "A captain for testing.", modifier: "Earn 1.5x gold."},
+    {captainID: "002", price: "50", name: "Test Captain #2", description: "A captain for testing.", modifier: "Improve morale by 50%"}
+];
