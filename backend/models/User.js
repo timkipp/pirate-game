532fa851fc8cee5bbcd70d3659d1232b1769ca0a
timkipp <timkipp@icloud.com>
2025-05-01 13:39:30 -0400

    Add User.js model file

---
+++ backend/models/User.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+// Define the User schema
+const UserSchema = new mongoose.Schema({
+  userName: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+// Export the model
+module.exports = mongoose.model('User', UserSchema);
